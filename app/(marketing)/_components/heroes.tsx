59456a5a578a08fc63d2afb85169efa5bfca742f
Dylan Ravel <dylanm.ravel@gmail.com>
2024-10-04 15:05:25 -0700

    Setup a bunch of starter files
    Im working on convex backend next

---
+++ app/(marketing)/_components/heroes.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export const Heroes = () => {
+    return (
+        <div className="flex flex-col items-center justify-center max-w-5xl">
+            <div className="flex items-center">
+                <div className="relative w-[100px] h-[100px] sm:w-[150px] sm:h-[150px] md:h-[200px] md:w-[200px]">
+                    <Image
+                        src="/logo-dark.svg"
+                        fill
+                        className="object-contain hidden dark:block"
+                        alt="logo"
+                    />
+                    <Image
+                        src="/logo.svg"
+                        fill
+                        className="object-contain dark:hidden"
+                        alt="logo"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
