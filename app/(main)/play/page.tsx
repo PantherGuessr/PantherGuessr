ef4c4044a32083606229f0a605a00cf852f4a4d4
Dylan Ravel <dylanm.ravel@gmail.com>
2024-10-05 20:50:25 -0700

    Fixed backend issue and main menu button

--- app/(main)/play/page.tsx
+++ app/(main)/play/page.tsx
@@ -2,25 +2,32 @@
 
 import { Footer } from "@/components/footer";
 import { Button } from "@/components/ui/button";
-import { Play } from "lucide-react";
+import { ArrowLeft, Play } from "lucide-react";
 import Link from "next/link";
 
 const PlayPage = () => {
     return (
         <div className="min-h-full flex flex-col">
-            <div className="flex flex-col items-center justify-center justify-start text-center gap-y-8 flex-1 px-6 pb-10">
-            <Link href="/game">
-                <Button variant="default" className="flex items-center">
-                    Play Singleplayer <Play className="w-4 h-4 ml-2" />
+            <div className="absolute mt-20 top-4 left-4">
+                <Link href="/">
+                    <Button variant="outline">
+                        <ArrowLeft className="h-4 w-4 mr-2" /> Go Back to Main Menu
+                    </Button>
+                </Link>
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-y-8 flex-1 px-6 pb-10">
+                <Link href="/game">
+                    <Button variant="default" className="flex items-center">
+                        Play Singleplayer <Play className="w-4 h-4 ml-2" />
+                    </Button>
+                </Link>
+                <Button
+                    variant="secondary" 
+                    className="flex items-center"
+                    onClick={() => alert('COMING SOON')}
+                >
+                    Play Multiplayer <Play className="w-4 h-4 ml-2" />
                 </Button>
-            </Link>
-            <Button
-                variant="secondary" 
-                className="flex items-center"
-                onClick={() => alert('COMING SOON')}
-            >
-                Play Multiplayer <Play className="w-4 h-4 ml-2" />
-            </Button>
             </div>
             <Footer />
         </div>
