13d805d6457b3c62e38509478ebeeacf57b7b8ef
Dylan Ravel <dylanm.ravel@gmail.com>
2024-10-05 23:49:12 -0700

    made header one component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,13 +4,13 @@ import React from "react";
 
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
-import { Logo } from "@/components/logo";
+import { Logo } from "./logo";
 import { Button } from "@/components/ui/button";
 import { useConvexAuth } from "convex/react";
 import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import { useUser } from "@clerk/clerk-react";
-import Spinner from "@/components/spinner";
 import { Shield } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const Navbar = () => {
     const { isAuthenticated, isLoading } = useConvexAuth();
@@ -19,13 +19,16 @@ export const Navbar = () => {
 
     return (
         <div className={cn(
-            "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+            "z-50 bg-background fixed top-0 flex items-center p-6 w-full",
             scrolled && "border-b shadow-sm"
         )}>
             <Logo />
-            <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+            <div className="ml-auto justify-end flex items-center gap-x-2">
                 {isLoading && (
-                    <Spinner />
+                    <>
+                    <Skeleton className="h-6 w-[100px]" />
+                    <Skeleton className="h-8 w-8 rounded-full" />
+                    </>
                 )}
                 {!isLoading && !isAuthenticated && (
                     <>
@@ -42,14 +45,14 @@ export const Navbar = () => {
                     </>
                 )}
                 {isAuthenticated && !isLoading && (
-                    <>
-                        {user?.publicMetadata?.role === "admin" ? (
-                            <div className="flex items-center justify- gap-x-1">
-                                <Shield fill="#FFD700" /> <p>{user?.username}</p>
-                            </div>
-                        ) : (
-                            <p>{user?.username}</p>
-                        )}
+                    <>                        {user?.publicMetadata?.role === "admin" ? (
+                        <div className="flex items-center justify- gap-x-1">
+                            <Shield fill="#FFD700" /> <p className="hidden md:flex">{user?.username}</p>
+                        </div>
+                    ) : (
+                        <p className="hidden md:flex">{user?.username}</p>
+                    )}
+
                         <UserButton
                             afterSignOutUrl="/"
                         />
