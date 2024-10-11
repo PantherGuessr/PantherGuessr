e8a43274203cb90a960c78db344e138114c2dbc2
dtsivkovski <dtsivkovs@gmail.com>
2024-10-11 15:47:18 -0700

    footer responsive dropdown menu

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,11 +2,12 @@ import { Button } from "@/components/ui/button"
 
 import { ModeToggle } from "@/components/mode-toggle"
 import Link from "next/link"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuPortal, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger } from "./ui/dropdown-menu"
 
 export const Footer = () => {
     return (
         <div className="flex items-center w-full p-6 bg-transparent z-5">
-            <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+            <div className="hidden md:flex md:ml-auto w-full justify-end items-center gap-x-2 text-muted-foreground">
                 {/* TODO: ADD a copyright message such as: © 2024 - PantherGuessr */}
                 <Link href="/credits">
                     <Button variant="ghost" size="sm">
@@ -23,6 +24,42 @@ export const Footer = () => {
                         Terms & Conditions
                     </Button>
                 </Link>
+                <ModeToggle />
+            </div>
+            <div className="flex md:hidden ml-auto w-full justify-between items-end text-muted-foreground">
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Button variant="ghost" size="sm">
+                            Additional
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="m-2">
+                        <Link href="/credits">
+                        <DropdownMenuItem>
+                            Credits
+                        </DropdownMenuItem>
+                        </Link>
+                        <DropdownMenuSub>
+                            <DropdownMenuSubTrigger>
+                                Legal
+                            </DropdownMenuSubTrigger>
+                            <DropdownMenuPortal>
+                            <DropdownMenuSubContent>
+                                <Link href="/privacy-policy">
+                                    <DropdownMenuItem>
+                                        Privacy Policy
+                                    </DropdownMenuItem>
+                                </Link>
+                                <Link href="/terms-and-conditions">
+                                    <DropdownMenuItem>
+                                        Terms & Conditions
+                                    </DropdownMenuItem>
+                                </Link>
+                            </DropdownMenuSubContent>
+                            </DropdownMenuPortal>
+                        </DropdownMenuSub>
+                    </DropdownMenuContent>
+                </DropdownMenu>
                 <ModeToggle />
             </div>
         </div>
