e28581b659987bb444c19d7f83e4b50e40969547
Dylan Ravel <dylanm.ravel@gmail.com>
2024-10-10 18:24:10 -0700

    Cooked credits page

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,6 +7,11 @@ export const Footer = () => {
     return (
         <div className="flex items-center w-full p-6 bg-transparent z-5">
             <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+                <Link href="/credits">
+                    <Button variant="ghost" size="sm">
+                        Credits
+                    </Button>
+                </Link>
                 <Link href="/privacy-policy">
                     <Button variant="ghost" size="sm">
                         Privacy Policy
