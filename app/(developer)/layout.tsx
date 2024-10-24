7a06a914cafe28d8d6d68f517e778ecfdfbc80a0
Dylan Ravel <dylanm.ravel@gmail.com>
2024-10-24 10:46:01 -0700

    Revert "Coded custom role checking status"
    This reverts commit c0408a3fee88133713acdaba70dc0df945f156d7.

--- app/(developer)/layout.tsx
+++ app/(developer)/layout.tsx
@@ -2,9 +2,9 @@
 
 import { Navbar } from "@/components/navbar";
 import { Skeleton } from "@/components/ui/skeleton";
-import { useRoleCheck } from "@/hooks/use-role-check";
-import { RedirectToSignIn } from "@clerk/nextjs";
-import { useConvexAuth } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { RedirectToSignIn, useUser } from "@clerk/nextjs";
+import { useConvexAuth, useQuery } from "convex/react";
 import { redirect } from "next/navigation";
 
 const AdminLayout = ({
@@ -12,11 +12,12 @@ const AdminLayout = ({
 }: {
     children: React.ReactNode;
 }) => {
-    const { isAuthenticated, isLoading: authLoading } = useConvexAuth();
-    const { data: isAdmin, isLoading: isAdminLoading } = useRoleCheck("admin");
+    const { isAuthenticated, isLoading } = useConvexAuth();
+    const { user } = useUser();
+    const isAdmin = useQuery(api.users.hasRole, { clerkId: user?.id || "", role: "admin" });
 
     // If they are loading
-    if(authLoading || isAdminLoading) {
+    if(isLoading) {
         return (
             <div className="h-full flex items-center justify-center">
                 <Skeleton className="w-[240px] h-[40px] translate-y-[40%]" />
