be6faf2c332031c73985cf246c85e81541d7fc71
dtsivkovski <dtsivkovs@gmail.com>
2024-10-23 19:23:40 -0700

    started data table for analytics levels page

--- app/(developer)/admin/_components/levels.tsx
+++ app/(developer)/admin/_components/levels.tsx
@@ -1,7 +1,20 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
-import { Table, TableBody, TableCell, TableHeader, TableRow } from "@/components/ui/table";
+import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+  } from "@tanstack/react-table"
+   
+  import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import Image from "next/image";
 import { useQuery } from "convex/react";
@@ -9,38 +22,125 @@ import { api } from "@/convex/_generated/api";
 import { useEffect, useState } from "react";
 import { Id } from "@/convex/_generated/dataModel";
 import { Skeleton } from "@/components/ui/skeleton";
+import { StorageId } from "convex/server";
+import { DataTable } from "./helpers/datatable";
+
+type Level = {
+    _id: Id<"levels">;
+    _creationTime: number;
+    title: string;
+    latitude: number;
+    longitude: number;
+    imageId: string;
+    timesPlayed: bigint;
+};
 
 
 const Levels = () => {
 
     const defaultImageSource = "/Invalid-Image.jpg";
 
+    // accessors and mutators for states
     const [clickedLevelId, setClickedLevelId] = useState<Id<"levels"> | null>(null);
     const [currentImageSrcUrl, setCurrentSrcUrl] = useState(defaultImageSource);
-    const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const [openDialogId, setOpenDialogId] = useState<Id<"levels"> | null>(null);
 
+    // convex api functions
     const tableData = useQuery(api.admin.getAllLevels);
     const imageSrc = useQuery(api.admin.getImageSrcByLevelId, clickedLevelId ? { id: clickedLevelId } : "skip");
 
+    // sets the image source to default on table data load
     useEffect(() => {
         if (tableData && tableData.length > 0) {
-            setClickedLevelId(tableData[0]._id);
+            setClickedLevelId(null);
         }
     }, [tableData]);
 
+    // updates image source state and open dialog id on dialog trigger
     useEffect(() => {
         if (imageSrc) {
             setCurrentSrcUrl(imageSrc);
+            setOpenDialogId(clickedLevelId); 
         }
-    }, [imageSrc]);
+    }, [imageSrc, clickedLevelId]);
 
+    // closes dialog
     const handleDialogClose = () => {
         setCurrentSrcUrl(defaultImageSource);
+        setOpenDialogId(null);
     };
+
+    // opens dialog
+    const handleDialogOpen = (levelId: Id<"levels">) => {
+        setClickedLevelId(levelId);
+    };
+
+    // creates image dialog button
+    function imageDialogCreator(row: Level) {return (
+        <Dialog open={openDialogId === row._id} onOpenChange={(open) => {
+            if (!open) {
+                handleDialogClose();
+            }
+        }}>
+            <DialogTrigger asChild>
+                <Button onClick={() => handleDialogOpen(row._id)}>
+                    View
+                </Button>
+            </DialogTrigger>
+
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        {row.title}
+                    </DialogTitle>
+                    <DialogDescription>
+                        Image ID: {row.imageId}
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="flex justify-center">
+                    {currentImageSrcUrl === "/Invalid-Image.jpg" ? (
+                        <Skeleton className="bg-zinc-400 dark:bg-red-900 w-full aspect-4/3" />
+                    ) : (
+                        <Image className="w-full" width="300" height="225" src={currentImageSrcUrl} alt={row.title} id={"image-" + row.imageId} />
+                    )}
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
+    }
+
+    // creates cell for image view dialog
+
+    // columns key value pairs for shadcn DataTable component
+    const columns: ColumnDef<Level>[] = [
+        {
+            accessorKey: "title",
+            header: "Title",
+        },
+        {
+            accessorKey: "_id",
+            header: "ID",
+            cell: (cell) => {
+                return cell.row.original._id;
+            }
+        },
+        {
+            accessorKey: "timesPlayed",
+            header: "Times Played",
+        },
+        {
+            accessorKey: "imageId",
+            header: "Image",
+            cell: (cell) => {
+                return imageDialogCreator(cell.row.original);
+            }
+        }
+    ]
 
     return (
         <>
-            <Table>
+            <DataTable columns={columns} data={tableData || []} />
+            {/* <Table>
                 <TableHeader>
                     <TableRow>
                         <TableCell>Title</TableCell>
@@ -94,7 +194,8 @@ const Levels = () => {
                         </TableRow>
                     ))}
                 </TableBody>
-            </Table>
+            </Table> */}
+
         </>
     );
 }
