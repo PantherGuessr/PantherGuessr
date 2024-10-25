192e64ace8c16d7065b4fdc3e56be8321e20c7ae
dtsivkovski <dtsivkovs@gmail.com>
2024-10-25 14:00:34 -0700

    implemented username editing boilerplate

--- app/(users)/profile/[USERNAME]/page.tsx
+++ app/(users)/profile/[USERNAME]/page.tsx
@@ -5,11 +5,14 @@ import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
 import { useUser } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
-import { Loader2, LucideShield, Shield, UserSearch } from "lucide-react";
+import { Loader2, LucideShield, Save, Shield, SquarePen, UserSearch, X } from "lucide-react";
 import "./backgrounds.css";
 import Image from "next/image";
 import Link from "next/link";
 import { Progress } from "@/components/ui/progress";
+import { useEffect, useState } from "react";
+import { Input } from "@/components/ui/input";
+import { set } from "date-fns";
 
 type Props = {
     params: { USERNAME: string }
@@ -19,6 +22,30 @@ const ProfilePage = ({ params }: Props) => {
     const usernameSubPage = params.USERNAME as string;
     const clerkUser = useUser();
     const user = useQuery(api.users.getUserByUsername, { username: usernameSubPage });
+
+    // username editing
+    const [usernameForUpdate, setUsernameForUpdate] = useState(user?.username);
+    const [isEditingUsername, setIsEditingUsername] = useState(false);
+    const [userNameInputWidth, setUserNameInputWidth] = useState(0);
+
+    // recalculate the width of the input based on the username length
+    useEffect(() => {
+        if (!usernameForUpdate) {
+            setUserNameInputWidth(64);
+        }
+        else {
+            if (usernameForUpdate.length > 32) {
+                setUsernameForUpdate(usernameForUpdate.slice(0, 32));
+            }
+            const width = usernameForUpdate.length * 19 + 64;
+            if (width < 64) {
+                setUserNameInputWidth(64);
+            }
+            else {
+                setUserNameInputWidth(width);
+            }
+        }
+    }, [usernameForUpdate]);
 
     if(user === undefined) {
         return (
@@ -66,7 +93,41 @@ const ProfilePage = ({ params }: Props) => {
                         <Image className="rounded-full translate-y-[-5em] mb-[-5em] md:mb-0 border-8 border-background" src={user.picture} width={200} height={200} alt="Profile Picture" />
                         <div className="flex flex-col items-center text-center md:text-start md:items-start justify-center gap-y-1">
                             <div className="flex md:flex-row flex-col items-center md:items-start">
-                                <h1 className="text-4xl font-bold md:pl-4 cursor-default">{user.username}</h1>
+                                <div className="flex flex-row items-center">
+                                    {isCurrentUser ? (isEditingUsername ? (
+                                        <>
+                                        <Input 
+                                            type="text" 
+                                            value={usernameForUpdate} 
+                                            onChange={(e) => setUsernameForUpdate(e.target.value)}
+                                            className="text-4xl font-bold md:pl-4 transition-all duration-[25] ease-in-out"
+                                            style={{ width: userNameInputWidth }}
+                                            onKeyDown={(e) => {
+                                                if(e.key === "Escape") {
+                                                    setIsEditingUsername(false);
+                                                }
+                                            }}
+                                        />
+                                        <X className="h-7 w-7 ml-1 mt-1 cursor-pointer" onClick={() => {
+                                            setIsEditingUsername(false);
+                                        }} />
+                                        <Save className="h-7 w-7 ml-1 mt-1 cursor-pointer" onClick={() => {
+                                            //TODO: implement saving that username
+                                            setIsEditingUsername(false);
+                                        }} />
+                                        </>
+                                    ) : (   
+                                        <> 
+                                        <h1 className="text-4xl font-bold md:pl-4 cursor-default">{user.username}</h1>
+                                        <SquarePen className="h-7 w-7 ml-1 mt-1 cursor-pointer" onClick={() => {
+                                            setIsEditingUsername(true)
+                                            setUsernameForUpdate(user.username);
+                                        }} />
+                                        </>  
+                                    )): (
+                                        <h1 className="text-4xl font-bold md:pl-4">{user.username}</h1>
+                                    )}
+                                </div>
                                 <div className="flex flex-row items-center md:items-start">
                                     {user.roles?.includes("admin") && (
                                         <Shield className="h-8 w-8 ml-3 mt-1" fill="#A50034" />
