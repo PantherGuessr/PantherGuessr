5438fcda2a39fb7621553fd57af0dc9b5a2283e2
Dylan Ravel <dylanm.ravel@gmail.com>
2024-10-25 15:03:47 -0700

    Refactored Clerk Image Domain
    In Next.js 14, the domains option is deprecated. I implemented a fix to use the new way of importing hostnames

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            'img.clerk.com'
-        ],
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: process.env.NEXT_PUBLIC_CONVEX_URL.replace(/^https?:\/\//, ''),
                 pathname: '/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'img.clerk.com',
+            }
         ],
     },
 };
