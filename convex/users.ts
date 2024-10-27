335962cbb5808783a890fa58080e335637f50292
Dylan Ravel <dylanm.ravel@gmail.com>
2024-10-27 11:25:58 -0700

    Improved badges in the front and back end

--- convex/users.ts
+++ convex/users.ts
@@ -167,7 +167,7 @@ export const hasChapmanEmail = query({
         if(!user) {
             return false;
         }
-
+        
         return user.emails.some(email => email.endsWith("@chapman.edu"));
     }
 });
