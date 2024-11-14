32d1c420ba8b77c7edab9483073c54b71c4e62c9
dtsivkovski <dtsivkovs@gmail.com>
2024-11-14 12:18:59 -0800

    new colors for cards

--- app/(additional)/credits/page.tsx
+++ app/(additional)/credits/page.tsx
@@ -16,7 +16,7 @@ const CreditsPage = () => {
       <div className="flex flex-col items-center justify-center text-center gap-y-8 flex-1 px-6 pb-10">
         <h1 className="text-4xl md:text-6xl justify-self-center">Credits</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-6xl mx-auto">
-          <Card className="md:hover:scale-105 hover:bg-[rgba(35,22,22,0.4)] transition-all backdrop-blur-sm drop-shadow-lg bg-[rgba(35,22,22,0.2)]">
+          <Card className="md:hover:scale-105 hover:bg-[rgba(35,22,22,0.07)] transition-all backdrop-blur-sm drop-shadow-lg bg-[rgba(35,22,22,0.03)]">
             <CardHeader>
               <Image className="rounded-lg mx-auto" src='/profile-pictures/dylan.jpeg' width={100} height={100} alt="Dylan" />
               <CardTitle className="pt-2">Dylan Ravel</CardTitle>
@@ -41,7 +41,7 @@ const CreditsPage = () => {
               </div>
             </CardHeader>
           </Card>
-          <Card className="md:hover:scale-105 hover:bg-[rgba(35,22,22,0.4)] transition-all backdrop-blur-sm drop-shadow-lg bg-[rgba(35,22,22,0.2)]">
+          <Card className="md:hover:scale-105 hover:bg-[rgba(35,22,22,0.07)] transition-all backdrop-blur-sm drop-shadow-lg bg-[rgba(35,22,22,0.03)]">
             <CardHeader>
               <Image className="rounded-lg mx-auto" src='/profile-pictures/daniel.jpeg' width={100} height={100} alt="Daniel" />
               <CardTitle className="pt-2">Daniel Tsivkovski</CardTitle>
@@ -66,7 +66,7 @@ const CreditsPage = () => {
               </div>
             </CardHeader>
           </Card>
-          <Card className="md:hover:scale-105 hover:bg-[rgba(35,22,22,0.4)] transition-all backdrop-blur-sm drop-shadow-lg bg-[rgba(35,22,22,0.2)]">
+          <Card className="md:hover:scale-105 hover:bg-[rgba(35,22,22,0.07)] transition-all backdrop-blur-sm drop-shadow-lg bg-[rgba(35,22,22,0.03)]">
             <CardHeader>
               <Image className="rounded-lg mx-auto" src='/profile-pictures/jake.jpeg' width={100} height={100} alt="Jake" />
               <CardTitle className="pt-2">Jake Milam</CardTitle>
