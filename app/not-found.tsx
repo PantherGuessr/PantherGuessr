19a9e3e9580a4b1a32e5da7600a0c0e16cb32495
Dylan Ravel <dylanm.ravel@gmail.com>
2024-11-18 14:07:48 -0800

    Added back button

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar";
 import { Button } from "@/components/ui/button";
-import { Home } from "lucide-react";
+import { ArrowLeft, Home } from "lucide-react";
 import Link from "next/link";
 
 export default function NotFound() {
@@ -14,10 +16,13 @@ export default function NotFound() {
             <h1 className="text-8xl">404</h1>
             <p className="text-xl">Page Does Not Exist</p>
             <Link href="/">
-              <Button>
-                <Home className="mr-2 w-4 h-4" /> Go Back Home
+              <Button variant="outline">
+                <Home className="mr-2 w-4 h-4" /> Go To Home Page
               </Button>
             </Link>
+            <Button variant="outline" onClick={() => window.history.back()}>
+              <ArrowLeft className="mr-2 w-4 h-4" /> Go Back To Previous Page
+            </Button>
           </div>
           <Footer />
         </div>
