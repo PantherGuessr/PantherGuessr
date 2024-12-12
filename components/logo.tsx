d5ba29c463d883879116e1e22c6de4f833eefa0e
Dylan Ravel <dylanm.ravel@gmail.com>
2024-12-11 20:57:33 -0800

    Small changes to the logo to make it work better

--- components/logo.tsx
+++ components/logo.tsx
@@ -17,23 +17,28 @@ interface LogoProps {
 export const Logo = ({ href, badge=true }: LogoProps) => {
   const logoContent = (
     <div className="flex items-center gap-x-2">
-      <Image 
+      <Image
+        draggable={false}
         src="/logo.svg"
         height="40"
         width="40"
         alt="Logo"
-        className="dark:hidden"
+        className="dark:hidden select-none"
       />
-      <Image 
+      <Image
+        draggable={false}
         src="/logo-dark.svg"
         height="40"
         width="40"
         alt="Logo"
-        className="hidden dark:block"
+        className="hidden dark:block select-none"
       />
-      <p className={cn("font-semibold p-2", font.className)}>PantherGuessr</p>
+      <p className={cn("font-semibold pl-2 select-none", font.className)}>PantherGuessr</p>
       {badge && (
-        <Badge className="h-6 bg-red-800 hover:bg-red-900 text-white cursor-default" >Alpha</Badge>
+        <Badge className={cn(
+          href == undefined ? "cursor-default hover:bg-red-800" : "cursor-pointer hover:bg-red-900",
+          "h-6 bg-red-800 text-white select-none"
+        )}>Alpha</Badge>
       )}
     </div>
   );
