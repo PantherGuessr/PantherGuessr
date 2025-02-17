1aa283dc4f903b4b8c4a0fdc8f2c08cb8e52c1f1
Dylan Ravel <dylanm.ravel@gmail.com>
2025-02-16 19:45:04 -0800

    updated TOS and privacy policy

--- app/(legal)/privacy-policy/page.tsx
+++ app/(legal)/privacy-policy/page.tsx
@@ -10,7 +10,7 @@ const PrivacyPolicyPage = () => {
         <div className="flex flex-col items-start justify-start md:text-justify gap-y-8 md:mt-10 md:mx-40 flex-1 px-6 pb-10">
           <h1 className="text-5xl">Privacy Policy</h1>
           <div className="space-y-4">
-            <p><span className="font-bold">Effective Date:</span> December 19th, 2024</p>
+            <p><span className="font-bold">Effective Date:</span> February 16th, 2025</p>
             <p>PantherGuessr, designated by (&quot;we&quot;, &quot;our&quot;, or &quot;us&quot;), is committed to protecting your privacy. This privacy policy explains how we collect, use, and share your information when you use our services.</p>
           </div>
 
@@ -37,7 +37,7 @@ const PrivacyPolicyPage = () => {
             <h2 className="text-2xl">4. User Rights and Data Requests</h2>
             <p><span className="font-bold">Security Measures:</span> We implement best practices to protect user data, including encrypted password handling through Clerk. However, we do not access user passwords directly.</p>
             <p><span className="font-bold">Account Deletion:</span> You can delete your account directly by navigating to &quot;Account Settings &gt; Security &gt; Delete Account&quot;.</p>
-            <p><strong>Additional Requests:</strong> For other data-related requests, such as accessing or modifying your data, please contact us at: <Link href="mailto:example@example.com" className="relative group text-muted-foreground hover:text-black hover:dark:text-white">example@example.com
+            <p><strong>Additional Requests:</strong> For other data-related requests, such as accessing or modifying your data, please contact us at: <Link href="mailto:pantherguessr@gmail.com" className="relative group text-muted-foreground hover:text-black hover:dark:text-white">pantherguessr@gmail.com
               <span className="absolute left-0 bottom-[-3px] w-0 h-[2px] bg-black dark:bg-white transition-all group-hover:w-full"></span>
             </Link>.</p>
           </div>
@@ -54,7 +54,7 @@ const PrivacyPolicyPage = () => {
 
           <div className="space-y-4">
             <h2 className="text-2xl">7. Contact Information</h2>
-            <p>For any privacy-related questions, please contact us at: <Link href="mailto:example@example.com" className="relative group text-muted-foreground hover:text-black hover:dark:text-white">example@example.com
+            <p>For any privacy-related questions, please contact us at: <Link href="mailto:pantherguessr@gmail.com" className="relative group text-muted-foreground hover:text-black hover:dark:text-white">pantherguessr@gmail.com
               <span className="absolute left-0 bottom-[-3px] w-0 h-[2px] bg-black dark:bg-white transition-all group-hover:w-full"></span>
             </Link></p>
           </div>
