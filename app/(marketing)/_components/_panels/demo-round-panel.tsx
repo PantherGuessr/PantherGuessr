f5f4399dad625b9b1edb9e4171d32ad1fe197cc4
dtsivkovski <dtsivkovs@gmail.com>
2025-02-21 12:51:13 -0800

    image update and hint location start

--- app/(marketing)/_components/_panels/demo-round-panel.tsx
+++ app/(marketing)/_components/_panels/demo-round-panel.tsx
@@ -7,11 +7,10 @@ import { cn } from "@/lib/utils";
 const DemoRoundPanel = () => {
   const theme = useTheme();
 
-  const darkHintIcon = "/new-user-heading/HintCircleDark.svg";
   const lightHintIcon = "/new-user-heading/HintCircle.svg";
 
   const hintOverlayClassName =
-    "absolute h-7 bottom-[60%] right-[26%] duration-300 bg-black dark:bg-white text-white dark:text-black text-sm text-center py-1 px-2 rounded-lg transition-opacity rounded-br-none drop-shadow-lg";
+    "absolute h-7 duration-300 bg-white text-black text-sm text-center py-1 px-2 rounded-lg transition-opacity rounded-br-none drop-shadow-lg";
 
   const [hint1Open, setHint1Open] = useState(false);
   // const [hint2Open, setHint2Open] = useState(false);
@@ -21,21 +20,24 @@ const DemoRoundPanel = () => {
   return (
     <div className="relative items-center justify-center w-full">
       <img
-        src={"/new-user-heading/DemoCampusImage.jpeg"}
+        src={"/new-user-heading/CampusDemoImage.jpeg"}
         alt={"PantherGuessr Demo Image 1"}
-        className={"object-contain aspect-4/3 rounded-lg border-4 border-primary"}
+        className={"object-contain aspect-4/3 rounded-lg border-4 border-primary bg-primary"}
       />
       <Image
-        className="absolute top-[42%] left-[75%] cursor-pointer hover:scale-125 transition-all drop-shadow-lg"
-        src={theme.theme == "dark" ? lightHintIcon : darkHintIcon}
+        className="absolute bottom-[25%] right-[11%] cursor-pointer hover:scale-125 transition-all drop-shadow-lg"
+        src={lightHintIcon}
         alt={"Hint 1"}
         onMouseOver={() => setHint1Open(true)}
         onMouseOut={() => setHint1Open(false)}
         height={20}
         width={20}
       />
-      <div id="hint1" className={cn(hintOverlayClassName, hint1Open ? "opacity-100" : "opacity-0")}>
-        DeMille Hall
+      <div
+        id="hint1"
+        className={cn(hintOverlayClassName, "bottom-[30%] right-[15%]", hint1Open ? "opacity-100" : "opacity-0")}
+      >
+        Info Booth
       </div>
     </div>
   );
