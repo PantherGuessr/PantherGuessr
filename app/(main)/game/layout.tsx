193a82d1077c948d5bf7dba0191cb2804b2ea23d
Dylan Ravel <dylanm.ravel@gmail.com>
2025-03-06 14:30:22 -0800

    Fixed hydration error by disabling hydration error

--- app/(main)/game/layout.tsx
+++ app/(main)/game/layout.tsx
@@ -3,7 +3,9 @@ import "./game.css";
 const GamePageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full w-full flex overflow-y-auto">
-      <main className="w-full">{children}</main>
+      <main suppressHydrationWarning className="w-full">
+        {children}
+      </main>
     </div>
   );
 };
