51024d6b9ac1426108e993d5bf47f55f0cc32342
Dylan Ravel <dylanm.ravel@gmail.com>
2026-04-05 13:20:24 -0700

    Reconfigured linting w/ eslint + prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,11 @@
 /* eslint-disable import/no-anonymous-default-export */
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
 import typescriptEslintEslintPlugin from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
-import importPlugin from "eslint-plugin-import";
 import prettier from "eslint-plugin-prettier";
-import reactUseProps from "eslint-plugin-react-use-props";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,8 +20,6 @@ export default [
   {
     plugins: {
       prettier,
-      "react-use-props": reactUseProps,
-      import: importPlugin,
     },
     rules: {
       "prettier/prettier": "error",
@@ -44,8 +40,6 @@ export default [
           jsx: "never",
         },
       ],
-      "import/order": "off",
-      "import/newline-after-import": "error",
     },
   },
   ...compat.extends("plugin:@typescript-eslint/recommended", "prettier").map((config) => ({
@@ -64,12 +58,12 @@ export default [
       "@typescript-eslint/explicit-function-return-type": "off",
       "@typescript-eslint/explicit-module-boundary-types": "off",
       "no-use-before-define": [0],
+      "@typescript-eslint/no-use-before-define": [1],
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-var-requires": "off",
-      "@typescript-eslint/no-use-before-define": "off",
     },
   },
   {
-    ignores: [".next/", "convex/_generated/", "next-env.d.ts"],
+    ignores: ["node_modules", ".next", "out", "build", "next-env.d.ts"],
   },
 ];
