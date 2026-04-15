141bb8205872ff2a02b74901a5aaf64b810db2e4
Dylan Ravel <dylanm.ravel@gmail.com>
2026-04-15 16:04:50 -0700

    Use pantherguessr logo in app metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,14 +24,8 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        media: "(prefers-color-scheme: light)",
-        url: "/logo.svg",
-        href: "/logo.svg",
-      },
-      {
-        media: "(prefers-color-scheme: dark)",
-        url: "/logo-dark.svg",
-        href: "/logo-dark.svg",
+        url: "/pantherguessr_logo.svg",
+        href: "/pantherguessr_logo.svg",
       },
     ],
   },
