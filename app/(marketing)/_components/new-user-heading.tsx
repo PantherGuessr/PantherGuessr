c2417f3fd214c0cdd63e1c0471f3cda72c57a5fa
Dylan Ravel <dylanm.ravel@gmail.com>
2026-04-15 18:26:39 -0700

    Fix landing page for unauth

--- app/(marketing)/_components/new-user-heading.tsx
+++ app/(marketing)/_components/new-user-heading.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { SignUpButton } from "@clerk/nextjs";
 import { ArrowRight } from "lucide-react";
+import { useMediaQuery } from "usehooks-ts";
 
 import DemoRoundPanel from "@/app/(marketing)/_components/_panels/demo-round-panel";
 import { Logo } from "@/components/logo";
@@ -9,10 +10,16 @@ import { cn } from "@/lib/utils";
 export const NewUserHeading = () => {
   const [signOnButtonHovered, setSignOnButtonHovered] = useState(false);
 
+  const isLarge = useMediaQuery("(min-width: 768px)");
+
   return (
     <>
       <div className="flex w-full items-center justify-center gap-x-4">
-        <Logo badge={false} logoDimensions={80} textOptions="text-2xl sm:text-5xl md:text-7xl font-semibold" />
+        {isLarge ? (
+          <Logo badge={false} logoDimensions={160} textOptions="text-2xl sm:text-5xl md:text-7xl font-semibold" />
+        ) : (
+          <Logo badge={false} logoDimensions={80} textOptions="text-2xl sm:text-5xl md:text-7xl font-semibold" />
+        )}
       </div>
       <div className="flex flex-grow flex-col items-center justify-center px-4 pt-4 sm:px-10 sm:pt-8 md:flex-row xl:px-20">
         <div className="flex basis-1/2 flex-col items-center justify-center px-4 transition-transform duration-300">
@@ -23,7 +30,7 @@ export const NewUserHeading = () => {
           </h2>
           <SignUpButton mode="modal" fallbackRedirectUrl={window.location.href}>
             <div
-              className="w-1/2 cursor-pointer transition-all duration-300 ease-in-out hover:w-[53%]"
+              className="w-[88%] cursor-pointer transition-all duration-300 ease-in-out hover:w-[100%] sm:w-3/4 sm:hover:w-[80%]"
               onMouseOver={() => setSignOnButtonHovered(true)}
               onMouseLeave={() => setSignOnButtonHovered(false)}
             >
