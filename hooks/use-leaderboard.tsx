20304e95a6f70f25b7a2e254ddb95f2377e82a82
Dylan Ravel <dylanm.ravel@gmail.com>
2026-04-20 17:11:40 -0700

    Only require userRank for clerk users

--- hooks/use-leaderboard.tsx
+++ hooks/use-leaderboard.tsx
@@ -46,7 +46,7 @@ export function useLeaderboard(type: LeaderboardType) {
     return [...topUsers, currentUser];
   }, [topUsers, currentUser, userInTop25]);
 
-  const isLoading = topUsers === undefined || currentUserProfile === undefined || userRank === undefined;
+  const isLoading = topUsers === undefined || currentUserProfile === undefined || (!!currentUser?.clerkId && userRank === undefined);
 
   return {
     topUsers: topUsers || [],
