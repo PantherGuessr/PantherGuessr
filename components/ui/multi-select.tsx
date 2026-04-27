8ff40d069740c6a8990272e5cd8a22f1c2fe477b
Dylan Ravel <dylanm.ravel@gmail.com>
2026-04-27 00:33:15 -0700

    Format + generated backend

--- components/ui/multi-select.tsx
+++ components/ui/multi-select.tsx
@@ -60,8 +60,7 @@ const multiSelectVariants = cva(
  * Props for MultiSelect component
  */
 interface MultiSelectProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof multiSelectVariants> {
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof multiSelectVariants> {
   options: {
     /** The text to display for the option. */
     label: string;
